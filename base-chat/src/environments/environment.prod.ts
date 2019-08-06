e25de181ab5027b4b541baf578d2e410a1671f3c
Marcus Jenkins <33973120+marcusjenkins92@users.noreply.github.com>
2019-08-05 21:38:33 -0400

    Add files via upload

---
+++ base-chat/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+	apiKey: 'AIzaSyBJh7_aJYQFJjmm7GBbdVSlxJD5KlamIgw',
+    authDomain: 'chat-demo-fd4ed.firebaseapp.com',
+    databaseURL: 'https://chat-demo-fd4ed.firebaseio.com',
+    projectId: 'chat-demo-fd4ed',
+    storageBucket: 'chat-demo-fd4ed.appspot.com',
+    messagingSenderId: '544816847509'
+  }
+};
